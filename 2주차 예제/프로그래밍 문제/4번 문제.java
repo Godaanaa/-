654a0723b915facd28d6de3eb8427f2e4bc184a5
Godaanaa <163219080+Godaanaa@users.noreply.github.com>
2024-03-24 17:40:27 +0900

    Update 4번 문제.java

--- 2주차 예제/프로그래밍 문제/4번 문제.java
+++ 2주차 예제/프로그래밍 문제/4번 문제.java
@@ -4,9 +4,9 @@ public class AAA {
 		int a;
 		System.out.print("초 단위 정수를 입력하세요: ");
 		a = in.nextInt();
-		int hours = a / 3600;
-		int minutes = a / 60;
-		int seconds = a % 60;
-		System.out.printf("%d시간 %d분 %d초", hours, minutes, seconds);
+		int b = a / 3600;
+		int c = a / 60;
+		int d = a % 60;
+		System.out.printf("%d시간 %d분 %d초", b, c, d);
 	}
 }
