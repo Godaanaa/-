7b2b007dd0bb5dba020660426a87822f659b0cb9
Godaanaa <163219080+Godaanaa@users.noreply.github.com>
2024-03-24 17:23:30 +0900

    Update 2번 문제.java

--- 2주차 예제/프로그래밍 문제/2번 문제.java
+++ 2주차 예제/프로그래밍 문제/2번 문제.java
@@ -1,6 +1,6 @@
 public class AAA {
 	public static void main(String[] args) {
-		int w, h, area;
+		int w;
 		Scanner in = new Scanner(System.in);
 		System.out.print("정수를 입력하세요 : ");
 		w = in.nextInt();
