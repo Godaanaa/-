c69647613ecf7baa31f2e981e3082585d881c9db
Godaanaa <163219080+Godaanaa@users.noreply.github.com>
2024-03-24 17:33:11 +0900

    Update 3번 문제.java

--- 2주차 예제/프로그래밍 문제/3번 문제.java
+++ 2주차 예제/프로그래밍 문제/3번 문제.java
@@ -1,7 +1,7 @@
 public class AAA {
 	public static void main(String[] args) {
 		int a, b;
-		double pi = 3.14; // 원주율 값
+		double pi = 3.14;
 		Scanner in = new Scanner(System.in);
 		System.out.print("원기둥의 밑면 반지름은? : ");
 		a = in.nextInt();
