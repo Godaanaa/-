31f8771dbd17077f51b474396bb655c364989984
Godaanaa <163219080+Godaanaa@users.noreply.github.com>
2024-03-24 17:29:03 +0900

    Create 3번 문제.java

---
+++ 2주차 예제/프로그래밍 문제/3번 문제.java
@@ -0,0 +1,13 @@
+public class AAA {
+	public static void main(String[] args) {
+		int a, b;
+		double pi = 3.14; // 원주율 값
+		Scanner in = new Scanner(System.in);
+		System.out.print("원기둥의 밑면 반지름은? : ");
+		a = in.nextInt();
+		System.out.print("원기둥의 높이는? : ");
+		b = in.nextInt();
+		double volume = pi * a * a * b;
+		System.out.printf("원기둥의 부피는 %.1f", volume);
+	}
+}
